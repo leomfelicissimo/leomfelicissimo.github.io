ef6d10c4c713591b7827d43a3445f3635c4d9c5a
leomfelicissimo <leonardo.felicissimo@sumup.com>
2020-03-22 17:18:08 -0300

    New site

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/leonardo/Projects/leomfelicissimo.github.io/src/layouts/index.js"
-  import data from "/Users/leonardo/Projects/leomfelicissimo.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
